96e978dfcbe7243de85bbbfa8e350c1750ca1cd9
Hadar <hadar.eli93@gmail.com>
2017-01-27 23:27:12 +0200

    Hadar Check 27.1

--- app/src/main/java/com/example/eliyahuha/trempy/MainActivity.java
+++ app/src/main/java/com/example/eliyahuha/trempy/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //thetjh
-        //lkj
+        // Hadar Check 27/1
     }
 }
